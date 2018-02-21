e2162e8ece6ef8c76d6c233d8ed09963ded58836
JiN <joonas.lauhala@cs.tamk.fi>
2018-02-21 23:57:48 +0200

    Minor changes

--- core/src/tiko/coregames/drilltothecore/managers/SettingsManager.java
+++ core/src/tiko/coregames/drilltothecore/managers/SettingsManager.java
@@ -1,7 +1,12 @@
 package tiko.coregames.drilltothecore.managers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+
 public class SettingsManager {
+    private Preferences preferences;
+
     public SettingsManager() {
-        // Not implemented
+        preferences = Gdx.app.getPreferences("DrillToTheCore.prefs");
     }
 }
