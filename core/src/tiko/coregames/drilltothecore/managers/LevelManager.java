ee2e9e887fcd8da1756620479486c667c3c04212
JiN <joonas.lauhala@cs.tamk.fi>
2018-02-21 18:32:30 +0200

    Minor changes

--- core/src/tiko/coregames/drilltothecore/managers/LevelManager.java
+++ core/src/tiko/coregames/drilltothecore/managers/LevelManager.java
@@ -1,7 +1,42 @@
 package tiko.coregames.drilltothecore.managers;
 
-public class LevelManager {
-    public LevelManager() {
-        // Not implemented
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TideMapLoader;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.Disposable;
+
+public class LevelManager implements Disposable {
+    private TiledMap tileMap;
+    private TiledMapRenderer levelRenderer;
+
+    public LevelManager(int level) {
+        initializeLevel(level);
+
+        levelRenderer = new OrthogonalTiledMapRenderer(tileMap);
+    }
+
+    private void initializeLevel(int level) {
+        TideMapLoader loader = new TideMapLoader();
+        StringBuilder path = new StringBuilder("leveldata/");
+
+        switch (level) {
+            default:
+                path.append("tutorial.tmx");
+                break;
+        }
+
+        tileMap = loader.load(path.toString());
+    }
+
+    public void renderLevel(OrthographicCamera camera, float delta) {
+        levelRenderer.setView(camera);
+        levelRenderer.render();
+    }
+
+    @Override
+    public void dispose() {
+        tileMap.dispose();
     }
 }
